fe5e7781bf899cfd626772a0323b767a95608d5c
Dilan Perera <r.dilanperera@gmail.com>
2020-10-02 22:26:15 +0530

    Update DB Add verification fields

--- db/exl_main.sql
+++ db/exl_main.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 30, 2020 at 06:32 AM
+-- Generation Time: Oct 02, 2020 at 06:55 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.7
 
@@ -24,6 +24,38 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `buyer`
+--
+
+CREATE TABLE `buyer` (
+  `userName` varchar(255) NOT NULL,
+  `buyerRate` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `buyer`
+--
+
+INSERT INTO `buyer` (`userName`, `buyerRate`) VALUES
+('admin', 0),
+('manthinima', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seller`
+--
+
+CREATE TABLE `seller` (
+  `userName` int(11) NOT NULL,
+  `mainRate` double NOT NULL,
+  `communicationRate` double NOT NULL,
+  `deliveringRate` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -34,8 +66,33 @@ CREATE TABLE `user` (
   `dob` date NOT NULL,
   `email` varchar(255) NOT NULL,
   `accountStatus` int(2) NOT NULL,
-  `verificationStatus` int(2) NOT NULL
+  `verificationStatus` int(2) NOT NULL,
+  `verificationOTP` int(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userName`, `firstName`, `lastName`, `dob`, `email`, `accountStatus`, `verificationStatus`, `verificationOTP`) VALUES
+('admin', 'Dilan', 'Perera', '2020-09-25', '2018cs123@stu.ucsc.cmb.ac.lk', 0, 1, 1403761),
+('manthinima', 'Manthi', 'Nimaya', '2020-08-17', 'nimayamanthi452@gmail.com', 0, 0, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `seller`
+--
+ALTER TABLE `seller`
+  ADD PRIMARY KEY (`userName`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userName`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
