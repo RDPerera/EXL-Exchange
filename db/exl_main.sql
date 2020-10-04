9a5432cdb345b266c045b57b1477a518c5aded8a
unknown <c.rathnayake97@gmail.com>
2020-10-05 02:31:45 +0530

    Added the advertisement table to the database

--- db/exl_main.sql
+++ db/exl_main.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 04, 2020 at 10:20 AM
--- Server version: 10.4.13-MariaDB
--- PHP Version: 7.4.7
+-- Host: 127.0.0.1:3308
+-- Generation Time: Oct 04, 2020 at 09:00 PM
+-- Server version: 8.0.18
+-- PHP Version: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -24,26 +25,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `buyer`
+-- Table structure for table `advertisement`
 --
 
-CREATE TABLE `buyer` (
-  `userName` varchar(255) NOT NULL,
-  `buyerRate` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `seller`
---
-
-CREATE TABLE `seller` (
-  `userName` varchar(255) NOT NULL,
-  `mainRate` double NOT NULL,
-  `communicationRate` double NOT NULL,
-  `deliveringRate` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+DROP TABLE IF EXISTS `advertisement`;
+CREATE TABLE IF NOT EXISTS `advertisement` (
+  `advertisementID` int(11) NOT NULL AUTO_INCREMENT,
+  `dateTime` datetime NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `image` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `tag` varchar(50) NOT NULL,
+  `content` varchar(1000) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `member1` varchar(255) DEFAULT NULL,
+  `member2` varchar(255) DEFAULT NULL,
+  `member3` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`advertisementID`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -51,33 +51,16 @@ CREATE TABLE `seller` (
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
   `userName` varchar(255) NOT NULL,
   `firstName` varchar(255) NOT NULL,
   `lastName` varchar(255) NOT NULL,
   `dob` date NOT NULL,
   `email` varchar(255) NOT NULL,
   `accountStatus` int(2) NOT NULL,
-  `verificationStatus` int(2) NOT NULL,
-  `verificationOTP` int(8) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `seller`
---
-ALTER TABLE `seller`
-  ADD PRIMARY KEY (`userName`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`userName`);
+  `verificationStatus` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
