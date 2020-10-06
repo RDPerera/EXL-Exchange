783e73dd531fc2321310872b9955553a50df4e54
unknown <c.rathnayake97@gmail.com>
2020-10-07 00:12:56 +0530

    created the viewAdvertisement file

---
+++ src/advertisements/viewAdvertisement.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View the advertisement</title>
+    <link rel="stylesheet" type="text/css" href="../css/viewAdvertisement.css" />
+</head>
+<body>
+
+    <div class="row">
+        <div class="column" >
+        asd
+        </div>
+        <div class="column">
+        asda
+        </div>
+    </div>
+</body>
+</html>
