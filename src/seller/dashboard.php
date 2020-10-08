4841146d92358774bf990032a9cd12437661027e
Dilan Perera <r.dilanperera@gmail.com>
2020-10-08 23:39:05 +0530

    Intergrate Create AD with

--- src/seller/dashboard.php
+++ src/seller/dashboard.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="../css/dashboard.css">
+    <link rel="stylesheet" type="text/css" href="../css/sdashboard.css">
     <link rel="stylesheet" type="text/css" href="../css/card.css" />
 
 </head>
