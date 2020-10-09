5c46b36543aa8a5b0fbfbcd606c61717aa99e4ad
unknown <c.rathnayake97@gmail.com>
2020-10-09 22:13:49 +0530

    finished the responsive homeNavBar

--- src/home/home.php
+++ src/home/home.php
@@ -18,27 +18,25 @@
 <body>
 
     <div class="topnav" id="myTopnav">
-        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <a href="javascript:void(0);" class="icon" onclick="scrollTheMenu()">
             <i class="fa fa-bars"></i> </a>
-        <img src="../img/logoDarkBlue.png">
+        <img src="../img/logoWhite.png">
         <a href="" class="active">Home</a>
         <a href="">Categories</a>
         <a href="">About</a>
         <a href="">Contact</a>
         <div class="buttons">
-        <button class="signin-button" href="">Sign In</button>
-        <button class="signup-button" href="">Sign Up</button>
-</div>
-        
+            <button class="signin-button" href="">Sign In</button>
+            <button class="signup-button" href="">Sign Up</button>
+        </div>
+
         <div class="search-container">
             <form action="">
                 <input type="text" placeholder="Search.." name="search">
                 <button type="submit"><i class="fa fa-search"></i></button>
             </form>
         </div>
-
     </div>
-
 
 
     <script type="text/javascript" src="../js/homeNavBar.js"></script>
--- src/js/homeNavBar.js
+++ src/js/homeNavBar.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function scrollTheMenu() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
