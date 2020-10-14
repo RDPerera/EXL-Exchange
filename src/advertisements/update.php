70c70c8126d369a0d35deb55e762d5f4999d3959
unknown <c.rathnayake97@gmail.com>
2020-10-15 00:19:30 +0530

    fixed database isses

--- src/advertisements/update.php
+++ src/advertisements/update.php
@@ -12,10 +12,10 @@
 
 <body>
 
-    <?php
+<?php
     
 //Database connection
-$db = mysqli_connect('localhost:3308', 'root', '', 'exl_main');
+$db = mysqli_connect('localhost', 'root', '', 'exl_main');
 
 // Check connection
 if($db === false){
