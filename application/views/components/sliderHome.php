8b2d19fe78f22dc82669baece9bfa1dfee41cc43
Dilan Perera <r.dilanperera@gmail.com>
2020-10-16 00:33:02 +0530

    Add Slider To View

---
+++ application/views/components/sliderHome.php
@@ -0,0 +1,24 @@
+
+<!-- the slideshow -->
+<div class="wholeSS">
+        <div class="slideshow-container">
+            <div class="mySlides fade">
+                <img src="../img/ss7.jpg" style="width:100%" class="slideshow">
+                <!-- <div class="text">The Exclusive Freelancing Platform for Undergraduates</div> -->
+            </div>
+            <div class="mySlides fade">
+                <img src="../img/ss8.jpg" style="width:100%" class="slideshow">
+                <!-- <div class="text">Join us today and enhance your skills</div> -->
+            </div>
+            <div class="mySlides fade">
+                <img src="../img/ss9.jpg" style="width:100%" class="slideshow">
+            </div>
+        </div>
+        <br>
+        <div style="text-align:center">
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+        </div>
+</div>
+<script type="text/javascript" src="../js/homeSlideShow.js"></script>   
