992d25d030e4d97b6d6e5f5e9aa9ad61a3a89436
Dilan Perera <r.dilanperera@gmail.com>
2020-10-16 00:33:55 +0530

    Add Home Page To View

---
+++ application/views/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EXL Exchange - The Freelancing Platform for Undergraduates</title>
+    <?php include "components/header-home.php"; ?>
+</head>
+<body>
+<?php include "components/nav-home.php"; ?>
+<?php include "components/slider-home.php"; ?>
+<?php include "components/footer-home.php"; ?>
+</body>
+</html>
