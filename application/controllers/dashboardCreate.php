86b5f36279c83cd0f858192389278f5974384a97
unknown <c.rathnayake97@gmail.com>
2020-10-16 22:39:58 +0530

    Added the back-end of the dashboardCreate

--- application/controllers/dashboardCreate.php
+++ application/controllers/dashboardCreate.php
@@ -3,15 +3,10 @@
 class dashboardCreate extends exlFramework
 {
  
-
   public function __construct()
   {
-
     $this->helper("linker");
-  
-    // $this->dashboardCreateModel = $this->model('testModel'); 
-
-
+    $this->advertisements = $this->model('advertisements'); 
   }
 
   public function index()
@@ -19,18 +14,8 @@ class dashboardCreate extends exlFramework
     $this->view("dashboardCreate");
   }
 
-  public function test_input($data)
-  {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-  }
-
   public function formHandling()
   {
-
-    //   $this->testModel->func('23','34');
 
     // session_start();
     // $complete = "display:none";
@@ -51,6 +36,7 @@ class dashboardCreate extends exlFramework
   
      ];
     
+
     $title = $category = $tag = $content = $status = $price = "";
 
     $ValidationErrors = 0;
@@ -58,9 +44,7 @@ class dashboardCreate extends exlFramework
       if (empty($_POST["title"])) {
         $errorData['titleErr'] = "* Title is required";
         $ValidationErrors++;
-      } else {
-        $title = test_input($_POST["title"]);
-      }
+      } 
 
       if (empty($_POST["category"])) {
         $errorData['categoryErr'] = "* Category is required";
@@ -70,82 +54,75 @@ class dashboardCreate extends exlFramework
       if (empty($_POST["tag"])) {
         $errorData['tagErr'] = "* Tag is required";
         $ValidationErrors++;
-      } else {
-        $tag = test_input($_POST["tag"]);
-      }
+      } 
 
       if (empty($_POST["content"])) {
         $errorData['contentErr']  = "* Content is required";
         $ValidationErrors++;
-      } else {
-        $content = test_input($_POST["content"]);
-      }
+      } 
 
       if (empty($_POST["status"])) {
         $errorData['statusErr'] = "* Status is required";
         $ValidationErrors++;
-      } else {
-        $status = test_input($_POST["status"]);
-      }
+      } 
 
       if (empty($_POST["price"])) {
         $errorData['priceErr'] = "* Price is required";
         $ValidationErrors++;
-      } else {
-        $price = test_input($_POST["price"]);
-      }
+      } 
     }
-
 
     if ($ValidationErrors == 0) { //there are no validation errors
 
-      if (isset($_POST['fsubmit'])) { //retrieveing user entered data from the form
-
-        
-
-        //   $title = mysqli_real_escape_string($db, $_POST['title']);
-        //   $category = mysqli_real_escape_string($db, $_POST['category']);
-        //   $status = mysqli_real_escape_string($db, $_POST['status']);
-        //   $tag = mysqli_real_escape_string($db, $_POST['tag']);
-        //   $content = mysqli_real_escape_string($db, $_POST['content']);
-        //   $member1 = mysqli_real_escape_string($db, $_POST['member1']);
-        //   $member2 = mysqli_real_escape_string($db, $_POST['member2']);
-        //   $member3 = mysqli_real_escape_string($db, $_POST['member3']);
-        //   $price = mysqli_real_escape_string($db, $_POST['price']);
-        //   $date = date('Y-m-d H:i:s'); //getting the current data and time
-
-
-        //   //taking an image as the user input and storing it
-        //   $target_dir = "../img/adUploads/";
-        //   $uploadOk = 0;
-        //   $image = "";
-        //   if (!empty($_FILES["imageupload"]["name"])) //if an image is selected
-        //   {
-        //     $filename = $_FILES["imageupload"]["name"];
-        //     $target_file = $target_dir . basename($_FILES["imageupload"]["name"]);
-        //     $uploadOk = 1;
-        //     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-        //   }
-
-        //   //if there were no errors
-        //   if ($uploadOk == 1) {
-        //     move_uploaded_file($_FILES["imageupload"]["tmp_name"], $target_file); //move the image file to the target location
-        //   }
-
-        //   if (!empty($_FILES["imageupload"]["name"])) //only if an image is selected
-        //   {
-        //     $timestamp = time();
-        //     $image = $userName . $timestamp . "." . $imageFileType; //generating an unique name to the image file
-        //     rename("../img/adUploads/$filename", "../img/adUploads/$image"); //adding the generated name to the file
-        //   }
-
-        //   $query = "INSERT INTO advertisement (dateTime,status,category,image,title,tag,content,userName,member1,member2,member3,price) VALUES ('$date','$status', '$category','$image' , '$title' , '$tag' ,'$content' , '$userName' , '$member1' , '$member2' , '$member3','$price')";
-        //   mysqli_query($db, $query);
-        //   $complete = "";
-        // }
-      }
+      if (isset($_POST['fsubmit'])){ 
+      
+        //retrieveing user entered data from the form
+          $title = $this->input('title');
+          $category = $this->input('category');
+          $tag = $this->input('tag');
+          $content = $this->input('content');
+          $status = $this->input('status');
+          $price = $this->input('price');
+          $member1 = $this->input('member1');
+          $member2 = $this->input('member2');
+          $member3 = $this->input('member3');
+          $date = date('Y-m-d H:i:s'); //getting the current data and time
 
 
+          //taking an image as the user input and storing it
+          $target_dir = "../public/assets/img/adUploads/";
+          $uploadOk = 0;
+          $image = "";
+          if (!empty($_FILES["imageupload"]["name"])) //if an image is selected
+          {
+            $filename = $_FILES["imageupload"]["name"];
+            $target_file = $target_dir . basename($_FILES["imageupload"]["name"]);
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+          }
+
+          //if there were no errors
+          if ($uploadOk == 1) {
+            move_uploaded_file($_FILES["imageupload"]["tmp_name"], $target_file); //move the image file to the target location
+          }
+
+          if (!empty($_FILES["imageupload"]["name"])) //only if an image is selected
+          {
+            $timestamp = time();
+            $image = $userName . $timestamp . "." . $imageFileType; //generating an unique name to the image file
+            rename("../public/assets/img/adUploads/$filename", "../public/assets/img/adUploads/$image"); //adding the generated name to the file
+          }
+
+          $this->advertisements->store($date,$status,$category,$image,$title,$tag,$content,$userName,$member1,$member2,$member3,$price);
+          $this->redirect("dashboardCreate");
+          // $complete = "";
+        }
+    }
+    else{ //there are validation errors
+      $this->view("dashboardCreate", $errorData);
+    }
+            
+      
       // $userCheck = "SELECT * FROM user WHERE userName='$userName' LIMIT 1";
       // $result = mysqli_query($db, $userCheck);
       // $user = mysqli_fetch_assoc($result);
@@ -168,10 +145,7 @@ class dashboardCreate extends exlFramework
       //   session_destroy();
       //   header('Location: ../login/login.php');
       // }
-    }
-    else{ //there are validation errors
-      $this->view("dashboardCreate", $errorData);
-    }
-  }
+  
 
-}
+      }
+    }
