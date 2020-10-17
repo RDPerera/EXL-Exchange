5bbdb011472a0a345882cfac0f911225fd09b1e9
Dilan Perera <r.dilanperera@gmail.com>
2020-10-18 00:45:31 +0530

    Add Popular/Recent/NewBie Ad sections UIs

--- application/views/components/sliderHome.php
+++ application/views/components/sliderHome.php
@@ -3,15 +3,15 @@
 <div class="wholeSS">
         <div class="slideshow-container">
             <div class="mySlides fade">
-                <img src="<?php srcIMG("ss7.jpg") ?>" style="width:100%" class="slideshow">
+                <img src="<?php echo srcIMG("ss7.jpg"); ?>" style="width:100%" class="slideshow">
                 <!-- <div class="text">The Exclusive Freelancing Platform for Undergraduates</div> -->
             </div>
             <div class="mySlides fade">
-                <img src="<?php srcIMG("ss8.jpg") ?>" style="width:100%" class="slideshow">
+                <img src="<?php echo srcIMG("ss8.jpg"); ?>" style="width:100%" class="slideshow">
                 <!-- <div class="text">Join us today and enhance your skills</div> -->
             </div>
             <div class="mySlides fade">
-                <img src="<?php srcIMG("ss9.jpg") ?>" style="width:100%" class="slideshow">
+                <img src="<?php echo srcIMG("ss9.jpg"); ?>" style="width:100%" class="slideshow">
             </div>
         </div>
         <br>
--- application/views/homeView.php
+++ application/views/homeView.php
@@ -1,17 +1,35 @@
 
 <?php include "components/headerHome.php"; ?>
 <?php include "components/sliderHome.php"; ?>
-<div class="main-title-create"><span class="blue-text-create">Top </span>Advertisements</div>
+
+<div class="main-title-create"><span class="blue-text-create">Popular </span>Advertisements</div>
 <section class='card-container'>
 <?php 
-//http://localhost/EXL-Exchange/assets/img/userImages/dilan.png
-    while ($row=mysqli_fetch_assoc($data))
+    while ($row=mysqli_fetch_assoc($data['popular']))
     {
         include "components/adCard.php";
     }    
 ?>
 </section>
 
+<div class="main-title-create"><span class="blue-text-create">Recent </span>Advertisements</div>
+<section class='card-container'>
+<?php 
+    while ($row=mysqli_fetch_assoc($data['new']))
+    {
+        include "components/adCard.php";
+    }    
+?>
+</section>
 
+<div class="main-title-create"><span class="blue-text-create">NewBie </span>Advertisements</div>
+<section class='card-container'>
+<?php 
+    while ($row=mysqli_fetch_assoc($data['noob']))
+    {
+        include "components/adCard.php";
+    }    
+?>
+</section>
 
 <?php include "components/footerHome.php"; ?>
