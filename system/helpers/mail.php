9839d994324359c9848722e7ec42463d9117b3f5
Dilan Perera <r.dilanperera@gmail.com>
2020-10-19 22:14:35 +0530

    ADD  Mail Lib as Linker

---
+++ system/helpers/mail.php
@@ -0,0 +1,40 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+function sendMail($to,$toName,$subject,$body,$altBody)
+{
+    // Load Composer's autoloader
+    require '../vendor/autoload.php';
+
+    // Instantiation of the PHPMailer
+    $mail = new PHPMailer(true);
+
+
+    //Server settings
+
+    $mail->isSMTP();                                            
+    $mail->Host       = 'smtp.gmail.com';                    
+    $mail->SMTPAuth   = true;                                   
+    $mail->Username   = 'exlexchangemail@gmail.com';                  
+    $mail->Password   = 'exl@1234';                              
+    $mail->SMTPSecure = "tls";        
+    $mail->Port       = 587;
+    $mail->setFrom('exlexchangemail@gmail.com', 'EXL-Exchange');
+    $mail->addAddress($to, $toName);
+    // Content
+    $mail->isHTML(true);
+    $mail->Subject = 'Reset Your Password';
+    $mail->Body    = $body;
+    $mail->AltBody = $altBody;
+
+
+    if($mail->send()){
+        return true;
+    }
+    else{
+        return false;
+    } 
+}
+?>
