cf81d0fdddc4f9c7335ddcab969f2b55480aa169
Dilan Perera <r.dilanperera@gmail.com>
2020-11-11 01:06:42 +0530

    Update Chat- Add Core Structure

--- application/controllers/chat.php
+++ application/controllers/chat.php
@@ -6,10 +6,23 @@ class chat extends exlFramework
         $this->helper("linker");
         $this->model=$this->model("chatModel");
     }
+
     public function index()
     {
-        $this->view("chatView");
+        $data['sender']=$sender="Dilan";
+        $data['receiver']=$receiver="Nimaya";
+        $data['chat']=$this->model->getChat($sender,$receiver);
+        $this->view("chatView",$data);
     }
-    
+    public function submit()
+    {
+        $sender="Dilan";
+        $receiver="Nimaya";
+        $message=$_POST['message'];
+        $date=date("Y-m-d");
+        $time=date("h:i:sa");
+        echo $time;
+        $this->model->send($message,$sender,$receiver,$date,$time);
+    }
 }
 ?>
