806639be38e49311b391ae57e7eadcb3bf837486
Dilan Perera <r.dilanperera@gmail.com>
2020-11-12 15:01:59 +0530

    Create Group Chat For Ad

---
+++ application/controllers/adChat.php
@@ -0,0 +1,37 @@
+<?php
+class adChat extends exlFramework
+{
+    public function __construct()
+    {
+        $this->helper("linker");
+        $this->model=$this->model("chatModel");
+    }
+
+    public function index()
+    {
+        $data['sender']=$sender="Dilan";
+        $data['receiver']=$receiver="Nimaya";
+        $data['chat']=$this->model->getChat($sender,$receiver);
+        $this->view("chatView",$data);
+    }
+    public function send()
+    {
+        $sender="Dilan";
+        $receiver="Nimaya";
+        $message=$_POST['message'];
+        date_default_timezone_set('Asia/Colombo');
+        $date=date("Y-m-d");
+        $time=date("h:i:sa");
+        echo $time;
+        $this->model->send($message,$sender,$receiver,$date,$time);
+    }
+    public function status()
+    {
+        $data['sender']=$sender="Dilan";
+        $data['receiver']=$receiver="dilan";
+        $data['chatStatus']=$this->model->getStatus($receiver);
+        $data['userDetails']=$this->model->getDetails($receiver);
+        $this->view("chatStatusView",$data);
+    }
+}
+?>
