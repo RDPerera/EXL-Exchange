da1b79327ce5e137b952c6fcff6c13f5bc84209e
Dilan Perera <r.dilanperera@gmail.com>
2020-11-13 02:00:50 +0530

    Update Request - Add Ad Details Header

--- application/views/jobRequestView.php
+++ application/views/jobRequestView.php
@@ -4,9 +4,22 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 </head>
 <body>
+
     <div class="main-container">
     <div class="massenger-container">
-        <div class="chat-top-header" id="chat-top-header"></div>
+        <div class="chat-top-header" >
+            <?php $data=$data['adDetails'];?>
+            
+            <div class="img-container"><img src="<?php echo adIMG($data['image']) ?>" class="addImg"></div>
+            <div class="title"> <?php echo $data['title'];?></div>
+            <div class="ad-rating">
+                <span class='feed-container'><?php echo "LKR ".$data['price']; ?></span>
+                <span class='rate-container'><img src="<?php echo icoIMG('icons8-star-96.png') ?>"class='ratingIcon'>
+                <span class='rating'><?php echo $data['rate']." | Feedbacks ".$data['feedbacks']; ?></span>
+            </div>
+            <div class="content"><?php echo $data['content'] ?></div>
+            
+        </div>
         <div class="chat-contacts" id="chat-header"></div>
         <div class="chat-main">
             <div class="chat-container">
