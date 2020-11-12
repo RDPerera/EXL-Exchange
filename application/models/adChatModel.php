7fcd7006db52f6ca76ea153de7c05f137d3df335
Dilan Perera <r.dilanperera@gmail.com>
2020-11-12 15:03:29 +0530

    Create Model and Views For Group Chat & Initiate Defalt Chat

---
+++ application/models/adChatModel.php
@@ -0,0 +1,30 @@
+
+<?php
+    class adChatModel extends database
+    {
+        public function send($message,$sender,$receiver,$date,$time)
+        {
+            $query = "INSERT INTO user_message 
+            VALUES('$receiver', '$sender', '$message','$date','$time')";
+            mysqli_query($GLOBALS['db'], $query);
+        }
+        public function getChat($sender,$receiver)
+        {
+            $chat = "SELECT * FROM user_message WHERE (sender='$sender' AND receiver='$receiver') OR (sender='$receiver' AND receiver='$sender')";
+            $result = mysqli_query($GLOBALS['db'], $chat);
+            return mysqli_fetch_all($result,MYSQLI_ASSOC);
+        }
+        public function getStatus($receiver)
+        {
+            $chatStatus = "SELECT * FROM user_online WHERE userName='$receiver' LIMIT 1";
+            $result = mysqli_query($GLOBALS['db'], $chatStatus);
+            return mysqli_fetch_assoc($result);
+        }
+        public function getDetails($user)
+        {
+            $userDetails = "SELECT * FROM user WHERE userName='$user' LIMIT 1";
+            $result = mysqli_query($GLOBALS['db'], $userDetails);
+            return mysqli_fetch_assoc($result);
+        }
+    }
+?>
---
+++ application/views/messangerView.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Request</title>
+    <?php linkCSS('chat'); ?>
+    <?php linkFav('ee-logo.png');?> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+</head>
+<body>
+    <div class="chat-header" id="chat-header">
+    </div>
+    <div class="chat-container">
+    <div class="chat-body" id="chat-body">
+    <div id="chat-container"></div>
+    </div>
+    <div class="message-input-container">
+        <input type="text" name="message" class="message-input" placeholder="Type a message..." id="message">
+        <button name="message-submit" class="message-submit" id="message-submit" >SEND</button>
+    </div>
+    </div> 
+
+    <script>
+    $(document).ready(function(){ 
+        fetchChat();
+        fetchStatus();
+        updateScroll();
+        var scrolled = false;
+        setInterval(function(){
+            $("#chat-body").on('scroll', function(){
+                scrolled=true;
+            });
+            if(!scrolled){
+                updateScroll();
+            }
+            fetchChat();
+            fetchStatus();
+        },100);
+
+        $(document).on('click', '.message-submit', function(){
+        var chat_message = $('#message').val();
+        if(chat_message!=""){
+            $.ajax({
+                url:"chat/send",
+                method:"POST",
+                data:{message:chat_message},
+                success:function(data){
+                    $('#message').val('');
+                    scrolled = false;
+                }
+            })
+            
+        }
+        });
+
+        function updateScroll(){
+            var element = document.getElementById("chat-body");
+            element.scrollTop = element.scrollHeight;
+        }
+        function fetchChat()
+        {
+            $.ajax({
+                url:"chat/index",
+                method:"POST",
+                success:function(data)
+                {
+                    $('#chat-container').html(data);
+                }
+            })
+        }
+        function fetchStatus()
+        {
+            $.ajax({
+                url:"chat/status",
+                method:"POST",
+                success:function(data)
+                {
+                    $('#chat-header').html(data);
+                }
+            })
+        }
+    });
+    </script>
+</body>
+</html>
