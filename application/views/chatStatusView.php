96253bab0eda37d9071439bfb647b648bb6e2eec
Dilan Perera <r.dilanperera@gmail.com>
2020-11-12 14:24:07 +0530

    Update Chat Add User Status

--- application/views/chatStatusView.php
+++ application/views/chatStatusView.php
@@ -5,15 +5,15 @@
         $user=$data['userDetails'];
         if($chat['status']=='1')
         {
-            $status="<svg class='blinking'><circle cx='5' cy='5' r='5' fill='white' /></svg> Online";
+            $status=" Online";
         }
         else
         {
             $status="Last Seen ".$chat['date_time'];
         }
         $render = "
-        <img src='".userIMG($user['profilePicture'])."' class='profile-picture'>
-        <div class='data-section'><span class='username'>".$user['firstName']." ".$user['lastName']."</span><span class='user-status'>".
+        <div class='data-section'><img src='".userIMG($user['profilePicture'])."' class='profile-picture'>
+        <span class='username'>".$user['firstName']." ".$user['lastName']."</span><span class='user-status'>".
         $status."</span></div>
         ";
         echo $render;
