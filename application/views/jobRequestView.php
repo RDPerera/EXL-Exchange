75d911555676a9472f739162f22db9326783582f
Dilan Perera <r.dilanperera@gmail.com>
2020-11-13 19:23:46 +0530

    Update Request-Add Countdown UI

--- application/views/jobRequestView.php
+++ application/views/jobRequestView.php
@@ -33,9 +33,63 @@
     </div>
     <div class="workspace-container">
         
+        <div class="workspace-head"><p>Request a Job</p></div>
+        <div class="timer-section">
+            <div class="countdown">
+                <ul>
+                <li><span id="days"></span>days</li>
+                <li><span id="hours"></span>Hours</li>
+                <li><span id="minutes"></span>Minutes</li>
+                <li><span id="seconds"></span>Seconds</li>
+                </ul>
+            </div>
+            <p>Time Countdown For JOB</p>
+            <div class="set-timer">
+            Set Date And Time For A JOB <input type="datetime-local" id="birthdaytime" name="birthdaytime">
+            </div>
+        </div>
     </div>
     </div>
     <script>
+    
+
+    (function () {
+        const second = 1000,
+                minute = second * 60,
+                hour = minute * 60,
+                day = hour * 24;
+
+            let birthday = "Sep 30, 2021 00:00:00",
+            countDown = new Date(birthday).getTime(),
+            x = setInterval(function() {    
+
+                let now = new Date().getTime(),
+                    distance = countDown - now;
+
+                document.getElementById("days").innerText = Math.floor(distance / (day)),
+                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+                //do something later when date is reached
+                if (distance < 0) {
+                let headline = document.getElementById("headline"),
+                    countdown = document.getElementById("countdown"),
+                    content = document.getElementById("content");
+
+                headline.innerText = "It's my birthday!";
+                countdown.style.display = "none";
+                content.style.display = "block";
+
+                clearInterval(x);
+                }
+                //seconds
+            }, 0)
+    }());
+
+
+
+
     $(document).ready(function(){ 
         fetchChat();
         fetchStatus();
