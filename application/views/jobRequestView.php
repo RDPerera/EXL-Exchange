b38a1d4ba284504e2adca1610d839957237cbbf7
Dilan Perera <r.dilanperera@gmail.com>
2020-11-15 00:39:07 +0530

    Add View -  JOB Request Module

--- application/views/jobRequestView.php
+++ application/views/jobRequestView.php
@@ -1,19 +1,20 @@
 <?php include "components/headerHome.php"; ?>
     <title>Job Request</title>
     <?php linkCSS('chat'); ?>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <?php linkJS('jquery.min'); ?>
 </head>
 <body>
 
-    <div class="main-container">
+    <div class="main-container" style="margin-bottom:30px">
     <div class="massenger-container">
         <div class="chat-top-header" >
             <?php $data=$data['adDetails'];?>
-            
             <div class="img-container"><img src="<?php echo adIMG($data['image']) ?>" class="addImg"></div>
             <div class="title"> <?php echo $data['title'];?></div>
             <div class="ad-rating">
-                <span class='feed-container'><?php echo "LKR ".$data['price']; ?></span>
+                <span class='feed-container'><?php echo "LKR ".$data['price'].".00"; ?></span>
+                <span class='adID-container'><?php echo "Advertisement Id : ".$data['advertisementID']; ?></span>
+                <span class='category-container'><?php echo "Category : ".$data['category']; ?></span>
                 <span class='rate-container'><img src="<?php echo icoIMG('icons8-star-96.png') ?>"class='ratingIcon'>
                 <span class='rating'><?php echo $data['rate']." | Feedbacks ".$data['feedbacks']; ?></span>
             </div>
@@ -32,7 +33,6 @@
         </div>
     </div>
     <div class="workspace-container">
-        
         <div class="workspace-head"><p>Request a Job</p></div>
         <div class="timer-section">
             <div class="countdown" id="countdown">
@@ -43,129 +43,36 @@
                 <li><span id="seconds"></span>Seconds</li>
                 </ul>
             </div>
-            <div class="set-timer">
-            <span>Set Seller Product Submission Date & Time </span>
-            <input type="date"  id="date" class="datetime" required>
-            <input type="time" id="time" class="datetime" required>
-            </div>
+            <form method="post" action="<?php echo BASEURL.'/jobRequest/send';?>">
+            <div class="set-timer" id="date-title-container">
+                <span id="date-title">Set Seller Product Submission Date & Time </span>
+                <div class="cluster" style="float:right">
+                    <input type="date"  id="date" class="datetime" step="any" name="job-date">
+                    <input type="time" id="time" class="datetime" step="any" name="job-time">
+                </div>
+                </div>
+                <div class="set-timer" id="price-title-container">
+                <span id='price-title'>Additional Payment For Seller</span>
+                <div class="cluster" style="float:right" id="price-section">Rs.<input type="text" id="ad-pay" class="datetime" name="additionalPayment" value="00.00"></div>
+                </div>
+                <div class="request-container">
+                    <div class="request-details">
+                        <table>
+                            <tr><td>Advertisement ID </td><td><?php echo $data['advertisementID']; ?></td></tr>
+                            <tr><td>Seller(Owner) User Name</td><td><?php echo $data['userName']; ?></td></tr>
+                            <tr><td>Submission DeadLine </td><td id="dead-line"></td></tr>
+                            <tr><td>Job Price</td><td>Rs.<span id="basic-payment"><?php echo $data['price'].".00"; ?></span></td></tr>
+                            <tr><td>Additional Payments</td><td id="add-payment"></td></tr>
+                            <tr><td>Total Payment</td><td id="total-payment"></td></tr>
+                        </table>
+                    </div>
+                    <div class="request-buttons">
+                        <input type="submit" class="request-ad" name="submit" value="Send Job Request">
+                    </div>
+                </div>
         </div>
+    </form>
     </div>
     </div>
-    <script>
-    function myfunc() {
-        let now = new Date();
-        var dd = now.getDate()+7;
-        var mm = now.getMonth()+1; 
-        var yyyy = now.getFullYear();
-        var h=now.getHours();
-        var m=now.getMinutes();
-        var s=now.getSeconds();
-        if (m < 10) {
-            m = "0" + m;
-        }
-        if (s < 10) {
-            s = "0" + s;
-        }
-        if (h < 10) {
-            h = "0" + h;
-        }
-        document.getElementById("time").value=h+':'+m+':'+s;
-        document.getElementById("date").value=yyyy+'-'+mm+'-'+dd;
-    }
-    myfunc();
-    (function () {
-        const second = 1000,
-            minute = second * 60,
-            hour = minute * 60,
-            day = hour * 24;
-        
-        var t= document. getElementById("date").value+'T'+document. getElementById("time").value,
-        countDown = new Date(t).getTime(),
-        x = setInterval(function() {
-            var now = new Date().getTime(),
-            t= document. getElementById("date").value+'T'+document. getElementById("time").value,
-            countDown = new Date(t).getTime(),
-            distance = countDown - now;
-            document.getElementById("days").innerText = Math.floor(distance / (day)),
-            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second); 
-            // later when date is reached change color to red
-            if (countDown < now) {
-            distance = now - countDown;
-            document.getElementById("countdown").style.color="red",
-            document.getElementById("days").innerText = Math.floor(distance / (day)),
-            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-            
-            }
-        }, 0)
-    }());
-
-
-
-
-    $(document).ready(function(){ 
-        fetchChat();
-        fetchStatus();
-        updateScroll();
-        var scrolled = false;
-        setInterval(function(){
-            $("#chat-body").on('scroll', function(){
-                scrolled=true;
-            });
-            if(!scrolled){
-                updateScroll();
-            }
-            fetchChat();
-            fetchStatus();
-        },100);
-
-        $(document).on('click', '.message-submit', function(){
-        var chat_message = $('#message').val();
-        if(chat_message!=""){
-            $.ajax({
-                url:"adChat/send",
-                method:"POST",
-                data:{message:chat_message},
-                success:function(data){
-                    $('#message').val('');
-                    scrolled = false;
-                }
-            })
-            
-        }
-        });
-
-        function updateScroll(){
-            var element = document.getElementById("chat-body");
-            element.scrollTop = element.scrollHeight;
-        }
-        function fetchChat()
-        {
-            $.ajax({
-                url:"adChat/index",
-                method:"POST",
-                success:function(data)
-                {
-                    $('#chat-container').html(data);
-                }
-            })
-            
-        }
-        function fetchStatus()
-        {
-            $.ajax({
-                url:"adChat/adstatus",
-                method:"POST",
-                success:function(data)
-                {
-                    $('#chat-header').html(data);
-                }
-            })
-        }
-    });
-    </script>
-</body>
-</html>
+    <?php linkJS("jobRequest"); ?>
+    <?php include "components/footerHome.php"; ?>
