819d562c08b918d09639db425cb64dcbcf0b30a2
Dilan Perera <r.dilanperera@gmail.com>
2020-11-15 19:13:32 +0530

    Add Buyer Request Section To Seller Dashboard

--- application/views/sellerDashboardView.php
+++ application/views/sellerDashboardView.php
@@ -8,7 +8,8 @@
     <?php linkCSS("sdashboard"); ?>
     <?php linkCSS("card"); ?>
     <?php linkFAV("ee-logo.png"); ?>
-
+    <?php $jobs=$data['jobs']; ?>
+    <?php $data=$data['memory'];?>
 </head>
 
 <body>
@@ -98,16 +99,41 @@
                     </a>
                 </div>
                 <div class="main-title"><span class="blue-text">JOB</span> Requests</div>
+                <?php 
+                        foreach ($jobs as $request){
+                            
+                ?>
+                <a href="<?php echo BASEURL.'/jobResponce/get/'.$request['jobId'];?>" style="text-decoration:none;color:#333;">
                 <div class="requests">
+                
                     <div class="request">
-                        <span><span class="request-title">Advertisemet ID</span>0215</span>
-                        <span><span class="request-title">Advertisement Title</span>I will write quality blog posts, SEO
-                            articles, and website content</span>
-                        <span><span class="request-title">Buyer Name</span>Nimaya Manthi</span>
-                        <span><span class="request-title">Request Status</span>Pending</span>
-                        <span><span class="request-title">Expaire Time/Date</span>2020 APR 10 10:30 AM</span>
+                        <span><span class="request-title">Advertisemet ID</span><?php echo $request['adId'] ?></span>
+                        <span><span class="request-title">Job ID</span><?php echo $request['jobId'] ?></span>
+                        <span><span class="request-title">Buyer Name</span><?php echo $request['userName'] ?></span>
+                        <span><span class="request-title">Request Status</span>
+                        <?php     
+                        if($request['jobStatus']=='0')
+                        {
+                            echo "<span style='color: #ffc107 ; font-weight:500'> Pending </span>";
+                        }
+                        else if($request['jobStatus']=='1')
+                        {
+                            echo "<span style='color: #00c241 ; font-weight:500'>  Accepted</span>";
+                        }
+                        else if($request['jobStatus']=='2')
+                        {
+                            echo "<span style='color: #d82303 ; font-weight:500'> Rejected </span>";
+                        }
+                        //when payment gateway is implemented this part should change
+                    ?>
+                        </span>
+                        <span><span class="request-title">Submission Date</span><?php echo $request['date']." ".$request['time']; ?></span>
+                        <span><span class="request-title">JOB Payment</span><?php echo $request['price']; ?></span>
+                        <span><span class="request-title">Additional Payment</span><?php echo $request['additionalPayment']; ?></span>
                     </div>
-                </div>
+                </div></a>
+                        <?php } ?>
+                
             </div>
         </div>
 
