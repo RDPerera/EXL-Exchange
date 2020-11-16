49b3314983e37f6d16cde66f1f5a8903e16e3358
NimayaPerera <54572453+NimayaPerera@users.noreply.github.com>
2020-11-16 21:00:30 +0530

    c1

---
+++ application/controllers/updateModerator.php
@@ -0,0 +1,32 @@
+<?php
+    class updateModerator extends exlFramework
+    {
+        public function __construct()
+        {
+            $this->helper("linker");
+            $this->model = $this->model('updateModeratorModel');
+        }
+
+        public function index()
+        {
+            $data=array();
+            $results=$this->model->fetchModerators();
+            $data['results']=$results;
+            $this->view("deleteModeratorView",$data);
+        }
+
+        public function update($id)
+        {          
+            $data = $this->model->fetchModeratorData($id);
+            $this->view("updateModeratorView",$data);
+            if(isset($_POST['firstname']))
+            {
+                $firstname= $_POST['firstname'];
+                $lastname = $_POST['lastname'];
+                $email = $_POST['email'];
+                $startdate= $_POST['startdate'];
+                $this->model->updateUser($firstname,$lastname,$email,$startdate,$id);
+            }
+        }
+    }
+?>
