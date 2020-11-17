1882022f7ee88e69a3e4eb2e1ec133b8de207e4e
Dilan Perera <r.dilanperera@gmail.com>
2020-11-17 22:52:18 +0530

    Add Moderator List For Moderator Dash Board

---
+++ application/controllers/allModerators.php
@@ -0,0 +1,19 @@
+<?php
+    class allModerators extends exlFramework
+    {
+        public function __construct()
+        {
+            $this->helper("linker");
+            $this->model = $this->model('deleteModeratorModel');
+        }
+
+        public function index()
+        {
+            $data=array();
+            $results=$this->model->fetchModerators();
+            $data['results']=$results;
+            $this->view("allModeratorView",$data);
+        }
+
+    }
+?>
