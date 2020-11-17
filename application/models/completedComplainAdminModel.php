7cd57b7297086e35cbf268da4b3ae261dfc48c92
Dilan Perera <r.dilanperera@gmail.com>
2020-11-17 22:54:36 +0530

    Add Moderator List And Current Complain List

--- application/models/completedComplainAdminModel.php
+++ application/models/completedComplainAdminModel.php
@@ -4,15 +4,37 @@ class completedComplainAdminModel extends database
     /* fetch all complain data where action status is 0 */
     public function fetchData()
     {
-        $query = "SELECT * FROM complain WHERE modId IS NOT NULL AND adminId IS NOT NULL";
+        $query = "SELECT * FROM complain WHERE (modId IS NOT NULL) AND (adminId IS NOT NULL)";
         $result = mysqli_query($GLOBALS['db'], $query);
         return mysqli_fetch_all($result,MYSQLI_ASSOC);
     }
-
+    public function getData()
+    {
+        $query = "SELECT * FROM complain WHERE (modId IS NOT NULL AND modId <> '') AND (adminId IS NOT NULL AND adminId <> '')";
+        $result = mysqli_query($GLOBALS['db'], $query);
+        return mysqli_fetch_all($result,MYSQLI_ASSOC);
+    }
+    public function fetchDataCur()
+    {
+        $query = "SELECT * FROM complain WHERE (modId IS NOT NULL) AND (adminId ='' OR adminId IS NULL)";
+        $result = mysqli_query($GLOBALS['db'], $query);
+        return mysqli_fetch_all($result,MYSQLI_ASSOC);
+    }
+    public function getDataCur()
+    {
+        $query = "SELECT * FROM complain WHERE (adminId ='' OR adminId IS NULL) AND (adminId ='' OR adminId IS NULL)";
+        $result = mysqli_query($GLOBALS['db'], $query);
+        return mysqli_fetch_all($result,MYSQLI_ASSOC);
+    }
       /*to approve the action status by moderator*/
     public function approve($modId,$actionStatus,$compID)
     {
-          $query="UPDATE complain SET  modID='$modId',actionStatus='$actionStatus' WHERE complainId = '$compID'";
+          $query="UPDATE complain SET  adminId='$modId',actionStatus='$actionStatus' WHERE complainId = '$compID'";
+          mysqli_query($GLOBALS['db'], $query);
+    }
+    public function apply($modId,$actionStatus,$compID)
+    {
+          $query="UPDATE complain SET  modId='$modId',actionStatus='$actionStatus' WHERE complainId = '$compID'";
           mysqli_query($GLOBALS['db'], $query);
     }
 }
