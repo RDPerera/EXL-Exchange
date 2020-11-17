931c6ce964574a96f80685fe2261c9892a4ef2a5
Dilan Perera <r.dilanperera@gmail.com>
2020-11-17 14:01:52 +0530

    Update Admin Dashboard - Add [Add-Moderator]

--- application/controllers/adminDashboard.php
+++ application/controllers/adminDashboard.php
@@ -5,6 +5,7 @@
         {
             $this->helper("linker");
             $this->model = $this->model('completedComplainAdminModel');
+            $this->amodel = $this->model('addModeratorModel');
         }
 
         public function index()
@@ -20,6 +21,20 @@
             $modId="1";
             $actionStatus=$_POST['action'];
             $user = $this->model->approve($modId,$actionStatus,$compID);
+        }
+        public function addModerator()
+        {
+            $data=array();
+            $this->view("addModeratorView",$data);
+        }
+
+        public function insert()
+        {
+            $firstname =  $_POST['firstname'];
+            $lastname =  $_POST['lastname'];
+            $email =  $_POST['email'];
+            $startdate =  $_POST['startDate'];
+            $user = $this->amodel->insertmod($firstname,$lastname,$email,$startdate);
         }
     }
 ?>
