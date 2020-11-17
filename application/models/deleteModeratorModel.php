e6b0eb96d2d174309888a566973480da9b5fb3e8
Dilan Perera <r.dilanperera@gmail.com>
2020-11-17 22:55:15 +0530

    Update Admin-Update Moderator Delete From User DB

--- application/models/deleteModeratorModel.php
+++ application/models/deleteModeratorModel.php
@@ -4,21 +4,14 @@ class deleteModeratorModel extends database
     /* Delete User from DB */
     public function deleteUser($id)
     {
-        $del = mysqli_query($GLOBALS['db'],"delete from moderators where id = '$id'");
+        mysqli_query($GLOBALS['db'],"delete from moderator where userName = '$id'");
+        mysqli_query($GLOBALS['db'],"delete from user where userName = '$id'");
     }
     
-    public function insertmod($firstname,$lastname,$email,$startdate)
-    {
-        $sql = "INSERT INTO moderators (firstname,lastname,email,startdate) VALUES ('$firstname' , 
-        '$lastname' , '$email' , '$startdate')";
-        $result = mysqli_query($GLOBALS['db'], $sql);
-    }
-
-
     /* fetch all moderators */
     public function fetchModerators()
     {
-        $query = "SELECT * FROM moderators";
+        $query = "SELECT DISTINCT * FROM moderator,user WHERE moderator.userName=user.userName";
         $result = mysqli_query($GLOBALS['db'], $query);
         return mysqli_fetch_all($result,MYSQLI_ASSOC);
     }
