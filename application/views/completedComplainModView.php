fc2cdd76d1020394bbf9dac451dfb1212509461a
Dilan Perera <r.dilanperera@gmail.com>
2020-11-19 11:59:10 +0530

    Update Admin/Mod Dashboard - Fix Redirecting Paths & Grammer Issues

--- application/views/completedComplainModView.php
+++ application/views/completedComplainModView.php
@@ -15,14 +15,14 @@
 <nav >
    <div class="item-image"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/ee-logo.png" class="logo"></div>
    <a href="<?php echo BASEURL.'/allModerators' ?>"><div class="item"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-user-resume-96.png" class="sidebar-icons"><span>Moderators</span></div></a>
-   <a href="<?php echo BASEURL.'/moderatorDashboard/current' ?>"><div class="item"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-complaint-90.png" class="sidebar-icons"><span>Current&nbspcomplains</span></div></a>
-   <a href="<?php echo BASEURL.'/moderatorDashboard' ?>"><div class="item selected" ><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-complaint-90.png" class="sidebar-icons"><span>Completed&nbspcomplains</span></div></a>
+   <a href="<?php echo BASEURL.'/moderatorDashboard/current' ?>"><div class="item"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-complaint-90.png" class="sidebar-icons"><span>Current&nbspcomplaints</span></div></a>
+   <a href="<?php echo BASEURL.'/moderatorDashboard' ?>"><div class="item selected" ><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-complaint-90.png" class="sidebar-icons"><span>Completed&nbspcomplaints</span></div></a>
    <a href="<?php echo BASEURL.'/moderatorDashboard' ?>"><div class="item"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-report-96.png" class="sidebar-icons"><span>Reports</span></div></a>
    <a href="<?php echo BASEURL.'/moderatorDashboard' ?>"><div class="item"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-pay-96.png" class="sidebar-icons"><span>Payments</span></div></a>
  </nav>
 <br><br>
 <div class="table-container">
-  <p class="title">Accomplished<span> Complains</span></p>
+  <p class="title">Accomplished<span> Complaint</span></p>
     <table >
       <tr>
         <th>Complain Id</th>
@@ -47,7 +47,49 @@
     <td><?php echo $row['jobId'] ; ?></td>
     <td><?php echo $row['advertisementId'] ; ?></td>
     <td><?php echo $row['complainType'] ; ?></td>
-    <td><?php echo $row['actionStatus'] ; ?></td>
+    <td>
+    
+      <select name="action" class="special-select">
+        <option value="0">No Action</option>
+        <option value="1">Account is BANNED for 1 Day</option>
+        <option value="2">Account is BANNED for 7 Days</option>
+        <option value="3">Account is BANNED for 14 Days</option>
+        <option value="4">Account is BANNED for 30 Days</option>
+        <option value="5">Account is BANNED for 60 Days</option>
+        <option value="6">Account is BANNED for 365 Days</option>
+        <option value="7">Account is BANNED for Permanatly</option>
+        <option value="8">Account is BLOCKED</option>
+      <?php
+        if ($row['actionStatus']=='1') {
+            echo "<option value='' selected='true'><option value=''>Account is BANNED for 1 Day</option >";
+        }
+        else if ($row['actionStatus']=='0') {
+          echo "<option value='' selected='true'>No Action</option >";
+      }
+        else if ($row['actionStatus']=='2') {
+            echo "<option value='' selected='true'>Account is BANNED for 7 Days</option >";
+        }
+        else if ($row['actionStatus']=='3') {
+            echo "<option value='' selected='true'>Account is BANNED for 14 Days</option >";
+        }
+        else if ($row['actionStatus']=='4') {
+            echo "<option value='' selected='true'>Account is BANNED for 30 Days</option >";
+        }
+        else if ($row['actionStatus']=='5') {
+            echo "<option value='' selected='true'>Account is BANNED for 60 Days</option >";
+        }
+        else if ($row['actionStatus']=='6') {
+            echo "<option value='' selected='true'>Account is BANNED for 365 Days</option >";
+        }
+        else if ($row['actionStatus']=='7') {
+            echo "<option value='' selected='true'>Account is BANNED for Permanatly</option >";
+        }
+        else {
+            echo "<option value='8' selected='true'>Account is BLOCKED<option >";
+        }  
+    ?>
+    </select>
+  </td>
     <td><?php echo $row['modId'] ; ?></td>
     <td><?php echo $row['adminId'] ; ?></td>
     
