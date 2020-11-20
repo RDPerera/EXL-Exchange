8abd45918d4ee54bd332995c8c7ba6c9af6deeef
Dilan Perera <r.dilanperera@gmail.com>
2020-11-20 16:36:05 +0530

    Configure The Database Server

--- config/config.php
+++ config/config.php
@@ -1,12 +1,18 @@
 <?php
 
 // Database configurations 
-define("HOST", "localhost");
-define("USER", "root");
-define("DATABASE", "exl_main");
-define("PASSWORD", "");
+define("HOST", "sql12.freemysqlhosting.net");
+define("USER", "sql12377730");
+define("DATABASE", "sql12377730");
+define("PASSWORD", "XLWKDaf4Zf");
+
+// define("HOST", "localhost");
+// define("USER", "root");
+// define("DATABASE", "exl_main");
+// define("PASSWORD", "");
+
 
 // Base URL
-define("BASEURL", "http://localhost/EXL-Exchange");
-
+define("BASEURL", "https://exl-exchange.herokuapp.com");
+// define("BASEURL", "http://localhost/EXL-Exchange");
 ?>
