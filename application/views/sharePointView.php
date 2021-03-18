3ebda09a67be0aa72785380a94b735b7993529cf
RDPerera <r.dilanperera@gmail.com>
2021-03-18 17:08:31 +0530

    ReInitiate ALL

--- application/views/sharePointView.php
+++ application/views/sharePointView.php
@@ -1,3 +1,7 @@
+<?php 
+$data["profilePic"]="dilan.png";
+$files=$data['files'];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +12,45 @@
     <?php linkCSS('sharePoint'); ?>
 </head>
 <body>
-    EXL Share Point
+    <div class="navbar">
+        <div class="head">
+        EXL Share Point
+        </div>
+        <div class="userimg">
+        <img class="profile-pic" src="<?php echo BASEURL;?>/public/assets/img/userImages/<?php if (isset($data["profilePic"])) {echo $data["profilePic"];} else {echo "pp_default.jpg";}?>">
+        </div>
+    </div>
+
+    <table>
+    <thead>
+        <th>ID</th>
+        <th>Filename</th>
+        <th>size (in mb)</th>
+        <th>Downloads</th>
+        <th>Action</th>
+    </thead>
+    <tbody>
+    <?php foreach ($files as $file): ?>
+        <tr>
+        <td><?php echo $file['id']; ?></td>
+        <td><?php echo $file['name']; ?></td>
+        <td><?php echo floor($file['size'] / 1000) . ' KB'; ?></td>
+        <td><?php echo $file['job_id']; ?></td>
+        <td><a href="<?php echo BASEURL."/public/assets/uploads/".$file['name']?>">Download</a></td>
+        </tr>
+    <?php endforeach;?>
+
+    </tbody>
+    </table>
+    <br><hr><br>
+    <div class="container">
+      <div class="row">
+        <form action="<?php echo BASEURL.'/sharePoint/uploadFile';?>" method="post" enctype="multipart/form-data" >
+          <h3>Upload File</h3>
+          <input type="file" name="myfile"> <br>
+          <button type="submit" name="save">upload</button>
+        </form>
+      </div>
+    </div>
 </body>
 </html>
