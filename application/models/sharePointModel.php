0a366b50659048f43a7de61db39c18ece7130769
RDPerera <r.dilanperera@gmail.com>
2021-03-18 17:14:45 +0530

    Add relevent DB func to basic upload download Share Point

--- application/models/sharePointModel.php
+++ application/models/sharePointModel.php
@@ -1,5 +1,26 @@
 <?php
 class sharePointModel extends database{
+    public function file($id)
+    {
+        $sql = "SELECT * FROM files WHERE id=$id";
+        $result = mysqli_query($GLOBALS['db'], $sql);
+        $file = mysqli_fetch_assoc($result);
+        return $file;
+    }
+    public function fileList()
+    {
+        $sql = "SELECT * FROM files";
+        $result = mysqli_query($GLOBALS['db'], $sql);
+        $files = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        print_r($files);
+        return $files;
+    }
     
+
+    public function fileUpload($name,$size,$download)
+    {
+        $sql = "INSERT INTO files (name, size, job_id) VALUES ('$name', $size, 1235)";
+        return mysqli_query($GLOBALS['db'], $sql);
+    }
 }
 ?>
