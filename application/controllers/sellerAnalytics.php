da60bf8441c31382786be024a887786b0d9a8e95
Chathura <c.rathnayake97@gmail.com>
2021-03-20 23:26:31 +0530

    Implemented the reporting structure

--- application/controllers/sellerAnalytics.php
+++ application/controllers/sellerAnalytics.php
@@ -6,18 +6,83 @@ class sellerAnalytics extends exlFramework
         $this->helper("linker");
         $this->sellerDashboardModel = $this->model('sellerDashboardModel');
     }
-    public function index()
+    public function index() //function to load the initial analytics page
     {
 
-        $userName = $_SESSION['userName'];
+        $userName = $_SESSION['userName'];  
         //retrieving user data from the database
         $user = $this->sellerDashboardModel->retrieveUser($userName);
+
         if ($user) {
 
-            $data[0] = $user['firstName'];
-            $data[1] = $user['lastName'];
-            $data[2] = $user['profilePicture'];
+            $data[0][0] = $user['firstName'];
+            $data[0][1] = $user['lastName'];
+            $data[0][2] = $user['profilePicture'];
+            $data[0][3] = $user['dob'];
+            $data[0][4] = $user['email'];
+
+            //retrieving seller data from the database
+            $user = $this->sellerDashboardModel->retrieveSeller($userName);
+            $data[0][5] = $user['mainRate'];
+            $data[0][6] = $user['communicationRate'];
+            $data[0][7] = $user['deliveringRate'];
+
+            //retriving user ad data from the model
+            $dataFromAds = $this->sellerDashboardModel->sellerAds($userName);
+
+            //entering the retrieved data to $data 2D array so that we can send it to the view
+            $i = 1;
+            if ($dataFromAds) {
+                $cardCount = count($dataFromAds); //number of ad records we received from the database 
+                if ($cardCount > 0) {
+                    while ($i <= $cardCount) {
+                        $j = 0;
+                        while ($j < 8) {
+                            $data[$i][$j] =  $dataFromAds[$i - 1][$j]; //putting those ad records to the $data 2D array
+                            $j++;
+                        }
+                        $i++;
+                    }
+                }
+            }
+            //load the view
+            
+            $this->view("s-dashboardANDanalytics", $data);
+        } else {
+            $this->redirect('login');
         }
-        $this->view("s-dashboardANDanalytics", $data);
+    }
+
+    public function loadStatPage($adID){ //to display the advertisement details on top
+
+        $userName = $_SESSION['userName'];  
+        //retrieving user data from the database
+        $user = $this->sellerDashboardModel->retrieveUser($userName);
+
+        if ($user) {
+
+            $data[0][0] = $user['firstName'];
+            $data[0][1] = $user['lastName'];
+            $data[0][2] = $user['profilePicture'];
+            $data[0][3] = $user['dob'];
+            $data[0][4] = $user['email'];
+
+            //retrieving seller data from the database
+            $user = $this->sellerDashboardModel->retrieveSeller($userName);
+            $data[0][5] = $user['mainRate'];
+            $data[0][6] = $user['communicationRate'];
+            $data[0][7] = $user['deliveringRate'];
+
+            //retrieving user ad data from the model
+            $dataFromAds = $this->sellerDashboardModel->retrieveAd($adID);
+           // print_r($dataFromAds);
+            $data[0][8] = $dataFromAds;
+ 
+            //load the view
+            
+            $this->view("s-dashboardAND_AdPerformance", $data);
+        } else {
+            $this->redirect('login');
+        }
     }
 }
--- application/views/s-dashboardANDanalytics.php
+++ application/views/s-dashboardANDanalytics.php
@@ -8,6 +8,7 @@
     <?php linkFAV("ee-logo.png"); ?>
     <?php linkCSS("sdashboard"); ?>
     <?php linkCSS("model") ?>
+    <?php linkCSS("card"); ?>
 </head>
 
 <body>
@@ -22,13 +23,13 @@
     <div class="sidebar">
         <center>
             <div class="sidebar-profile-container">
-                <a href="<?php echo BASEURL; ?>/sellerdashboard/loadChangeDPView"><img src="<?php echo BASEURL; ?>/public/assets/img/userImages/<?php if ($data[2]) {
-                                                                                                                                                    echo $data[2];
+                <a href="<?php echo BASEURL; ?>/sellerdashboard/loadChangeDPView"><img src="<?php echo BASEURL; ?>/public/assets/img/userImages/<?php if ($data[0][2]) {
+                                                                                                                                                    echo $data[0][2];
                                                                                                                                                 } else {
                                                                                                                                                     echo "pp_default.jpg";
                                                                                                                                                 } ?>" class="sidebar-profile"></a>
             </div>
-            <span class="slidbar-name"><?php echo $data[0] . " " . $data[1]; ?></span>
+            <span class="slidbar-name"><?php echo $data[0][0] . " " . $data[0][1]; ?></span>
         </center>
         <div class="sidebar-menu">
             <a href="<?php echo BASEURL; ?>/sellerdashboard"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-home-144.png" class="sidebar-icons "><span>Home</span></a>
@@ -36,15 +37,75 @@
             <a href="<?php echo BASEURL; ?>/sellerJob/pending"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/pending.png " class="sidebar-icons"><span>Pending Jobs</span></a>
             <a href="<?php echo BASEURL; ?>/sellerJob/active"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/active.png " class="sidebar-icons"><span>Active Jobs</span></a>
             <a href="<?php echo BASEURL; ?>/sellerJob"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-submit-resume-96.png " class="sidebar-icons"><span>All Job Request</span></a>
-            <a href="<?php echo BASEURL; ?>/advertisements_controller" class="selected-item"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-plus-math-96.png" class="sidebar-icons"><span>Create Advertisement</span></a>
-            <a href="<?php echo BASEURL; ?>/sellerAnalytics"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-report-96.png" class="sidebar-icons"><span>Analytics</span></a>
+            <a href="<?php echo BASEURL; ?>/advertisements_controller" ><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-plus-math-96.png" class="sidebar-icons"><span>Create Advertisement</span></a>
+            <a href="<?php echo BASEURL; ?>/sellerAnalytics" class="selected-item"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-report-96.png" class="sidebar-icons"><span>Analytics</span></a>
             <a href="#"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-question-mark-96.png" class="sidebar-icons"><span>Help & Support</span></a>
             <a href="<?php echo BASEURL; ?>/sellercomplaint"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-complaint-90.png" class="sidebar-icons"><span>Complaints</span></a>
             <a href="#"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-settings-500.png" class="sidebar-icons"><span>Settings</span></a>
         </div>
     </div>
 
+    <div class="content-super">
+        <div class="page-container">
+            <div class="content">
+                <div class="main-title"><span class="blue-text">Advertisement</span> Performance</div>
+                <div class="advertisements">
+                    <?php
+                    $cardCount = count($data) - 1;
+                    if ($cardCount > 0) {
+                        $i = 1;
+                        while ($i <= $cardCount) {
+                            if (!$data[$i][1]) //to check whether user have inserted an advertisement cover or not
+                            {
+                                $data[$i][1] = 'AdvertisementDefault.jpg'; // if not assign the default image
+                            }
+                            echo "<a href='" . BASEURL . "/sellerAnalytics/loadStatPage/" . $data[$i][0] . "' style='text-decoration:none;color:black'>
+                                <div class='card'>
+                                    <img src='" . BASEURL . "/public/assets/img/adUploads/" . $data[$i][1] . "' class='card-image' />
+                                    <div class='card-info'>
+                                        <div class='card-title'>
+                                            " . $data[$i][2] . "
+                                        </div>
+                                        <div class='card-category'>
+                                            Category <span class='card-tag'>" . $data[$i][3] . "</span>
+                                        </div>
+                                        <div class='card-feedback'>
+                                            Feedbacks <span class='card-feedback-number'>+" . $data[$i][4] . "</span>
+                                        </div>
+                                        <div class='card-rate'>
+                                            Rate
+                                            <span class='card-rate-number'><img src='" . BASEURL . "/public/assets/img/icons/icons8-star-96.png'
+                                                    class='rate-star' />" . $data[$i][5] . "</span>
+                                        </div>
+                                        <div class='card-description'>
+                                        " . $data[$i][6] . "
+                                        </div>
+                                    </div>
+                                    <div class='card-price'>
+                                        <span class='card-price-tag'>LKR " . $data[$i][7] . "</span>
+                                    </div>
+                                </div>
+                                </a>
+                                ";
+                            $i++;
+                        }
+                    }
+                    ?>
+                    <a class="nostyle" href="<?php echo BASEURL; ?>/advertisements_controller">
+                        <!--  clear anchor tag styles -->
+                        <div class="empty-card">
+                            <img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-plus-math-96.png" width="50px" height="50px" style="vertical-align: middle; padding-right: 10px" />
+                            Create An Advertisement
 
+                        </div>
+                    </a>
+                </div>
+                <div class="main-title"><span class="blue-text">Earnings</span> Statistics</div>
+
+            </div>
+        </div>
+
+    </div>
 </body>
 
 </html>
