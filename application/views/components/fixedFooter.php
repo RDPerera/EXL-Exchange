f2a7b247b369947f67d3060a9a365a7c729d31f4
RDPerera <r.dilanperera@gmail.com>
2021-03-26 16:04:55 +0530

    Add FB,Insta,Twitter Links for Footer

--- application/views/components/fixedFooter.php
+++ application/views/components/fixedFooter.php
@@ -3,9 +3,9 @@
 <div class="footer-container-x">
     <span class="footer-text"> © EXL-Exchange 2020</span>
     <span class="social-media">
-        <a href="#"><i class="fab fa-facebook-square"></i></a>
-        <a href="#"><i class="fab fa-instagram"></i></a>
-        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="https://www.facebook.com/EXL-Exchange-109671294549011"><i class="fab fa-facebook-square"></i></a>
+        <a href="https://www.instagram.com/exlexchange/"><i class="fab fa-instagram"></i></a>
+        <a href="https://twitter.com/ExchangeExl"><i class="fab fa-twitter"></i></a>
     </span>
 </div>
 </footer>
