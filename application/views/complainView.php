3708e5510918e6f3ff7100871eceb66e9f663dae
RDPerera <r.dilanperera@gmail.com>
2021-03-30 15:21:40 +0530

    Add complaint for buyer

--- application/views/complainView.php
+++ application/views/complainView.php
@@ -9,8 +9,59 @@
     <?php linkCSS("card"); ?>
 	<?php linkFAV("ee-logo.png"); ?>
 	<?php linkCSS('add-moderator'); ?>
-	<?php $jobs=$data['joblist'];?>
-    <?php $data=$data['memory'];?>
+    <?php linkCSS('helpSubmit');?>
+<style>
+    .container{
+        width:100%;
+        height:100vh;
+        display:flex; 
+        justify-content:center;  
+    }
+    .tbl{
+        padding:10px;
+        align-self:center;
+        
+    }
+    .title
+    {
+        color: #007bff;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        font-size: x-large;
+        font-weight: 600;
+        margin-bottom: 10px;
+    }
+    .issue,.submit,.description
+    {
+        padding:10px;
+        border-radius: 7px;
+        border:none;
+        background-color: rgb(238, 238, 238);
+    }
+    .submit
+    {
+        float:right;
+        padding:10px;
+        border-radius: 7px;
+        border:none;
+        color:white;
+        background-color:#007bff ;
+        transition: ease-in-out 0.3s;
+    }
+    .submit:hover{
+        color:#007bff;
+        background-color:rgb(238, 238, 238) ;
+    }
+    tr,td{
+        padding:10px;
+    }
+    .success{
+        color:green;
+        font-weight:600;
+    }
+    body{
+        font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+    }
+</style>
 </head>
 
 <body>
@@ -34,64 +85,50 @@
         </center>
         <div class="sidebar-menu">
             <a href="<?php echo BASEURL; ?>/sellerDashboard" ><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-home-144.png" class="sidebar-icons "><span>Home</span></a>
-            <a href="#"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-chat-96.png" class="sidebar-icons"><span>Messages</span></a>
             <a href="<?php echo BASEURL; ?>/sellerJob/pending"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/pending.png " class="sidebar-icons"><span>Pending Jobs</span></a>
             <a href="<?php echo BASEURL; ?>/sellerJob/active"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/active.png " class="sidebar-icons"><span>Active Jobs</span></a>
             <a href="<?php echo BASEURL; ?>/sellerJob"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-submit-resume-96.png " class="sidebar-icons"><span>All Job Request</span></a>
             <a href="<?php echo BASEURL; ?>/advertisements_controller"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-plus-math-96.png" class="sidebar-icons"><span>Create Advertisement</span></a>
             <a href="<?php echo BASEURL; ?>/sellerAnalytics"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-report-96.png" class="sidebar-icons"><span>Analytics</span></a>
             <a href="#"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-question-mark-96.png" class="sidebar-icons"><span>Help & Support</span></a>
-            <a href="<?php echo BASEURL; ?>/sellercomplaint" class="selected-item"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-complaint-90.png" class="sidebar-icons"><span>Complaints</span></a>
-            <a href="#"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-settings-500.png" class="sidebar-icons"><span>Settings</span></a></div>
+            <a href="<?php echo BASEURL; ?>/complaint" class="selected-item"><img src="<?php echo BASEURL; ?>/public/assets/img/icons/icons8-complaint-90.png" class="sidebar-icons"><span>Complaints</span></a>
+            </div>
     </div>
 
 <div class="content-super">
     <div class="page-container">
-  	<div class="form-style-10">
-		<h1 >Complain Form</h1>
-		<div class="inner-wrap">
-			<form method="post"   action="<?php echo BASEURL.'/sellerComplaint/insert';?>"> 
-				<label style='display:none' for="complain User">Complainant: </label>
-				<input style='display:none' type="text" id="complainUser" name="complainUser" value="<?php echo $data[0][8]; ?>" >
-		</div>
-		<br>
-		<div class="inner-wrap "  class="<?php echo BASEURL.'/complain/selectad';?>">
-			<label for="accuseduser">Complainee:</label>
-			<input type="text" placeholder="Enter User Name" name="complainee" id="complainee">
+    <form method="post"  action="<?php echo BASEURL.'/complain/insert';?>" enctype="multipart/form-data">
+<div class="container">
+<input type="hidden" name="complainerUsername" value="<?php echo $data['complainerUsername']; ?>">
+<table class="tbl">
+    <tr class="srule"><td class="scolumn" colspan="2"><p class="title">complain Form</p></td></tr>
+    <tr class="rule"><td class="column">Issue</td><td class="column2">
+        <select name="issue" id="issue" class="issue">
+            <option value="Account Issue" class="op" selected>Select complain Type</option>
+            <option value="Fraud" class="op">Fraud</option>
+            <option value="Abuse" class="op">Abuse</option>
+            <option value="Seller Rule Violation" class="op">Seller Rule Violation</option>
+			<option value="Buyer Rule Violation" class="op">Buyer Rule Violation</option>
+            <option value="Failures" class="op">Failures</option>
         </select>
-		</div>
-		<br>
-		<div class="inner-wrap">
-			<label for="complainType">Complain Type</label>
-			<br>
-			<br>
-            <select id="complainType" name="complainType" class="dropdown" style="color:white;box-shadow:0 0 2px #333">
-                <option value = "Fraud">Fraud</option>
-                <option value = "Abuse">Abuse</option>
-                <option value = "Seller rule violation">Seller rule violation</option>
-				<option value = "Failures">Failures</option>
-				<option value = "Buyer rule violation">Buyer rule violation</option>
-              	</select>
-		</div>
-		<br>
-        <div class="inner-wrap">
-			<label for="description">Description:</label>
-			<br>
-			<textarea style="background-color:#F2F2F2;border:none;border-radius:5px;width:580px" name="description" id="description" cols="30" rows="10"></textarea>
-		</div>
-		<br>
-		
-		<div class="inner-wrap">
-		<div class="button-section">
-			<input type="submit" name="submit" >
-		</div>
-																						</div>
-		</form>
-	    </div>
-	</div>
-	</div>
-
+    </td></tr>
+	<tr class="rule"><td class="column">Accuesed UserName</td><td class="column2">
+        <input type="text" name="accusedusername" id="accusedusername" class="accusedusername">
+    </td></tr>
+    <tr class="rule"><td class="column">Descrition</td><td class="column2">
+        <textarea name="description" id="description" cols="30" rows="10" class="description"></textarea>
+    </td></tr>
+	<tr class="rule"><td class="column">Job Id</td><td class="column2">
+	<input type="text" name="jobid" id="jobid" class="jobid">
+    </td></tr>
+	<tr class="rule"><td class="column">Advertisment Id Id</td><td class="column2">
+	<input type="text" name="adid" id="adid" class="adid">
+    </td></tr>
+    <tr class="rule"><td class="column"></td><td class="column2">
+	 <input type="submit" name="submit" id="submit" value="Make a complaint" class="submit">
+    </td></tr>
+</table>
+<form>
 </div>
 </body>
-
 </html>
